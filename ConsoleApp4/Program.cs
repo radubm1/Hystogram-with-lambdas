840412f78016d47d7106705943336cc4f4585438
Radu <Radu@DESKTOP-V9TMDUL>
2022-08-28 18:30:41 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Hystogram example
+
+var arr = new int[] {1,2,3,4,4,4,5,5,5,6,7,7};
+
+var hst = arr.GroupBy(x => x).Select(x => new { Key = x.Key, Value = x.Count() }).OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x=> x.Value);
+
+foreach (var h in hst)  
+    Console.WriteLine(String.Join(" ", h));
